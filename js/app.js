398dab38ed3bcb2fb72504113a78696a6d46010b
Serhii Myshko <sergeiown@gmail.com>
2023-01-05 00:26:17 +0200

    add text editing ability by Ctrl+E and saving by Ctrl+S

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
 "use strict";
 
-import * as minimum from "./functions.js";
+const textDisplay = document.getElementById("text-display");
+const textEditor = document.getElementById("text-editor");
+
+document.addEventListener("keydown", actionOnEvent);
+
+let isEditing = false;
+
+function actionOnEvent(event) {
+  if (event.ctrlKey && event.key === "e") {
+    event.preventDefault(); // Disable default behavior for Ctrl+E
+    if (!isEditing) {
+      textEditor.value = textDisplay.innerText;
+      textDisplay.style.display = "none";
+      textEditor.style.display = "block";
+      isEditing = true;
+    }
+  } else if (event.ctrlKey && event.key === "s") {
+    event.preventDefault(); // Disable default behavior for Ctrl+S
+    if (isEditing) {
+      textDisplay.innerText = textEditor.value;
+      textEditor.style.display = "none";
+      textDisplay.style.display = "block";
+      isEditing = false;
+    }
+  }
+}
